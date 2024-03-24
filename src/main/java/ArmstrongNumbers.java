f3f810990015f09bbdef9efb8d0bc4e1c7540738
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-24 13:53:27 -0300

    Refactor isArmstrongNumber.

--- src/main/java/ArmstrongNumbers.java
+++ src/main/java/ArmstrongNumbers.java
@@ -1,18 +1,10 @@
-import java.util.ArrayList;
-
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
-        if (numberToCheck == 0) return true;
-
+        var digits = String.valueOf(numberToCheck).length();
         double sum = 0.0;
-        var numberList = new ArrayList<Integer>();
-
         for (int n = numberToCheck; n != 0; n /= 10)
-            numberList.add(n % 10);
-
-        for (int number : numberList)
-            sum += Math.pow(number, numberList.size());
+            sum += Math.pow(n % 10, digits);
 
         return sum == numberToCheck;
     }
