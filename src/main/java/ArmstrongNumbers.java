96e134ea6b1e73024e1086f706399e2df40e6aa0
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-03-24 13:47:54 -0300

    Exercise solution.

--- src/main/java/ArmstrongNumbers.java
+++ src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
+        if (numberToCheck == 0) return true;
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        double sum = 0.0;
+        var numberList = new ArrayList<Integer>();
 
+        for (int n = numberToCheck; n != 0; n /= 10)
+            numberList.add(n % 10);
+
+        for (int number : numberList)
+            sum += Math.pow(number, numberList.size());
+
+        return sum == numberToCheck;
     }
 
 }
